99fe1720f57229e2f25417079ab47ac28be96ace
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-05-30 03:33:58 -0400

    updated a class name change

--- src/expand.js
+++ src/expand.js
@@ -31,9 +31,8 @@ function expandSideBar() {
 
 function expandTodo() {
     todoContent.addEventListener('click', function(e) {
-        console.log(e.target);
         if (e.target.className == 'todo-container') {
-            e.target.children[1].classList.toggle('hide-todo');
+            e.target.children[1].classList.toggle('hide-element');
         }
     })
 }
