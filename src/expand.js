b1a432fd54a80a89af1240829270dfb90cdcd67b
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-06-06 00:24:33 -0400

    fixed bug that prevented dynamic elements from collapsing

--- src/expand.js
+++ src/expand.js
@@ -3,7 +3,6 @@ export {expandSideBar, expandTodo}
 const expandButton = document.querySelector('.navbar-brand');
 const sideBar = document.querySelector('aside');
 const logoContainer = document.querySelector('.logo-container');
-const sideBarText = document.querySelectorAll('.link-text');
 const logoName = document.querySelectorAll('.logo-name');
 const mainContent = document.querySelector('main');
 const navBar = document.querySelector('nav');
@@ -14,6 +13,8 @@ const expandedTodo = document.querySelector('.todo-container-expanded');
 
 function expandSideBar() {
     expandButton.addEventListener('click', function() {
+        const sideBarText = document.querySelectorAll('.link-text');
+
         sideBar.classList.toggle('side-bar-expanded');
         sideLinks.classList.toggle('side-links-expanded');
         sideProjects.classList.toggle('side-projects-expanded');
@@ -22,10 +23,10 @@ function expandSideBar() {
         navBar.classList.toggle('nav-expanded');
         sideBarText.forEach((e) => {
             e.classList.toggle('hide-text');
-        })
+        });
         logoName.forEach((e) => {
             e.classList.toggle('hide-text');
-        })
+        });
     });
 }
 
@@ -34,5 +35,5 @@ function expandTodo() {
         if (e.target.className == 'todo-container') {
             e.target.children[1].classList.toggle('hide-element');
         }
-    })
-}
+    });
+};
