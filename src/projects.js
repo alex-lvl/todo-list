03e9a46ea15370f1cbbf9826aa1a89388b59d684
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-06-09 22:16:05 -0400

    fixed bug that deleted incorrect project form when button was clicked

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,31 @@
-export {displayProjectForm}
+export {activateProjectTabListeners}
 
-const projectsList = document.querySelector('.side-projects')
+const projectHeader = document.querySelector('header');
+const projectFormTab = document.querySelector('.side-projects-header');
+const projectTabs = document.querySelector('.side-projects')
+const todoContent = document.querySelector('.content');
 const myProjects = [];
 
-function displayProjectForm() {
-    createProjectForm();
+function activateProjectTabListeners() {
+    projectFormTab.addEventListener('click', () => {
+        createProjectForm();
+    });
+    
+    projectTabs.addEventListener('click', e => {
+        let project = e.target.closest('.project');
+        let lis = [...projectTabs.children];
+        let index = lis.indexOf(project);
+    
+        if(e.target.classList.contains('cancel-project-icon')) {
+            myProjects.splice(lis.indexOf(e.target.parentElement),1);
+            e.target.parentElement.remove();
+            console.log(myProjects);
+            console.log('deleted project form');
+        } else if (project) {
+            removeContent();
+            updateProjectHeader(index);
+        }
+    });
 }
 
 class Project {
@@ -28,8 +49,10 @@ class Project {
 }
 
 function createProjectForm() {
+    const project = new Project(undefined,undefined)
+    myProjects.push(project);
+    
     const li = document.createElement('li')
-    li.className = 'project';
 
     const titleInput = document.createElement('input');
     titleInput.className = 'project-form';
@@ -43,15 +66,31 @@ function createProjectForm() {
     </svg>`;
     li.appendChild(titleInput);
 
-    projectsList.appendChild(li);
+    projectTabs.appendChild(li);
 
     titleInput.addEventListener('keypress', function(e) {
         if (e.key === 'Enter') {
-            const project = new Project(titleInput.value)
+            project.title = titleInput.value;
             project.createProject(li);
-            myProjects.push(project);
+            li.className = 'project';
             console.log('submitted');
             console.log(myProjects);
         }
     });
+}
+
+function updateProjectHeader(i) {
+    projectHeader.innerHTML = `
+    <h1>
+        <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-box project-icon" viewBox="0 0 16 16">
+            <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+        </svg>
+        <span class="text-capitalize">${myProjects[i].title}</span>
+    </h1>`;
+}
+
+function removeContent() {
+    while (todoContent.firstChild) {
+        todoContent.removeChild(todoContent.firstChild);
+      }
 }
